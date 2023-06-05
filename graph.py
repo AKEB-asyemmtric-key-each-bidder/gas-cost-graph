0b6480afd4aa3f41bfe875cf996175c7a12f8b9f
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-06-04 21:14:10 -0700

    inital graph code created

---
+++ graph.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Generate some data points for the functions
+x = np.linspace(1, 50, 10000)
+y1 = 103435 + 4834050/x
+y2 = 122480 + 2167802/x
+
+# Plot the functions
+plt.plot(x, y1, label='Function 1: x^2')
+plt.plot(x, y2, label='Function 2: x^3')
+
+# Add a legend
+plt.legend()
+
+# Add labels and title to the plot
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
+plt.title('Comparison of the Proposed Methodology and Galal')
+
+# Display the plot
+plt.show()
