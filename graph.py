407499da87a7d8c0b0da0acbf2e54aa3840f9cc7
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-06-04 21:56:01 -0700

    labels added

--- graph.py
+++ graph.py
@@ -2,20 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Generate some data points for the functions
-x = np.linspace(1, 50, 10000)
+x = np.linspace(1, 30, 10000)
 y1 = 103435 + 4834050/x
 y2 = 122480 + 2167802/x
 
 # Plot the functions
-plt.plot(x, y1, label='Function 1: x^2')
-plt.plot(x, y2, label='Function 2: x^3')
+plt.plot(x, y1, label='Succinctly Verifiable Sealed-bid Auction')
+plt.plot(x, y2, label='Proposed Methodology')
 
 # Add a legend
 plt.legend()
 
 # Add labels and title to the plot
-plt.xlabel('x-axis')
-plt.ylabel('y-axis')
+plt.xlabel('Number of bidders')
+plt.ylabel('Gas cost')
 
 # Display the plot
 plt.show()
